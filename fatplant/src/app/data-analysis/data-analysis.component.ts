817b9c7a90e8968399eac1c0c8ad6375ecffcc60
AllanLu <384054882@qq.com>
2019-09-27 10:12:26 -0500

    add data html

--- fatplant/src/app/data-analysis/data-analysis.component.ts
+++ fatplant/src/app/data-analysis/data-analysis.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-data-analysis',
   templateUrl: './data-analysis.component.html',
   styleUrls: ['./data-analysis.component.css']
 })
 export class DataAnalysisComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  private blast: string;
+  private isVisibale: boolean;
+  constructor() {
+    // let childprocess=require('child_process');
+    // let options:string[]=[];
+    // cp.exec('python',function (err,stdout) {console.log(stdout);});
   }
 
+  ngOnInit() {
+    this.blast = "input blast here";
+    this.isVisibale = false;
+  }
+  debug(){
+    // console.log(msg);
+    let options:string[]=[];
+    //this.childProcessService.childProcess.exec("python",options,(data) => {console.log(data);});
+
+  }
+  SetVisiable(){
+    this.isVisibale=!this.isVisibale;
+  }
 }
