4320a96d3fe1c893cf98f7b3e11953ae89984445
Jonah Marz <jmarz793@yahoo.com>
2019-10-24 22:53:21 -0500

    Fatty Acid table

--- fatplant/src/app/fattyacid/fattyacid.component.ts
+++ fatplant/src/app/fattyacid/fattyacid.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-fattyacid',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FattyacidComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['picture','name','mass','sofa_id','other_names','delta_notation'];
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  dataSource:MatTableDataSource<any>;
+  constructor(private afs:AngularFirestore) { }
 
   ngOnInit() {
+    let docs=this.afs.collection('Fatty Acid',ref=>ref.limit(100)).valueChanges().subscribe(data =>{
+        this.dataSource=new MatTableDataSource(data)
+        this.dataSource.paginator = this.paginator;
+    })
   }
+
+  applyFilter(filterValue: string) {
+      filterValue = filterValue.trim();
+      filterValue = filterValue.toLowerCase();
+      this.dataSource.filter = filterValue;
+    }
 
 }
