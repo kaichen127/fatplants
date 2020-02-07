b49809ae96a48b88b2ee20535d9cde77cdbe8915
unknown <brockweekley@gmail.com>
2020-02-06 21:51:18 -0600

    Started Style Rework

--- fatplant/src/app/homepage/homepage.component.ts
+++ fatplant/src/app/homepage/homepage.component.ts
@@ -3,18 +3,18 @@ declare var require: any;
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-  partner1 = require("../assets/DD.png");
-  partner2 = require("../assets/partner2.jpg");
-  partner3 = require("../assets/partner3.jpg");
-  partner4 = require("../assets/WU.png");
-  partner5 = require("../assets/partner5.jpg");
-  partner6 = require("../assets/partner6.jpg");
-  image1 = require("../assets/1.png");
-  image2 = require("../assets/2.png");
-  image3 = require("../assets/3.png");
+  partner1 = require('../assets/DD.png');
+  partner2 = require('../assets/partner2.jpg');
+  partner3 = require('../assets/partner3.jpg');
+  partner4 = require('../assets/WU.png');
+  partner5 = require('../assets/partner5.jpg');
+  partner6 = require('../assets/partner6.jpg');
+  image1 = require('../assets/1.png');
+  image2 = require('../assets/2.png');
+  image3 = require('../assets/3.png');
   constructor() { }
 
   ngOnInit() {
