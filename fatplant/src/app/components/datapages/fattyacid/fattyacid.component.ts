2c09d1b568aa5be59dd67403f0800478dce689bc
Jonah Marz <jmarz793@yahoo.com>
2020-02-11 13:30:18 -0600

    Changed Directory Structure

---
+++ fatplant/src/app/components/datapages/fattyacid/fattyacid.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { FirestoreConnectionService } from '../services/firestore-connection.service';
+
+@Component({
+  selector: 'app-fattyacid',
+  templateUrl: './fattyacid.component.html',
+  styleUrls: ['./fattyacid.component.css']
+})
+export class FattyacidComponent implements OnInit {
+
+  displayedColumns = ['picture','name','mass','sofa_id','other_names','delta_notation'];
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  dataSource:MatTableDataSource<any>;
+  constructor(private db:FirestoreConnectionService) { }
+
+  ngOnInit() {
+    let docs=this.db.connect('Fatty Acid').subscribe(data =>{
+        this.dataSource=new MatTableDataSource(data)
+        this.dataSource.paginator = this.paginator
+    })
+  }
+
+  applyFilter(filterValue: string) {
+      filterValue = filterValue.trim();
+      filterValue = filterValue.toLowerCase();
+      this.dataSource.filter = filterValue;
+    }
+
+}
