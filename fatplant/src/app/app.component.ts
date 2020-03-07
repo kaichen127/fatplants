896ea69a1cbc6a21c1f2d7a0d4cdc64728ee5f00
Matt Hudson <mrh4hd@mail.missouri.edu>
2020-03-07 16:19:55 -0600

    Add opaque nav bar transition

--- fatplant/src/app/app.component.ts
+++ fatplant/src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fatplant';
+
+  scrollMarker;
+  scrollObserver: IntersectionObserver;
+  opaque: boolean = false;
+
+  ngOnInit() {
+    this.scrollMarker = document.querySelector('#scroll-marker');
+    this.scrollObserver = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio <= 0) {
+          this.setOpaque();
+          console.log('opaque');
+        }
+        else {
+          this.setTransparent();
+          console.log('transparent');
+        }
+      });
+    });
+    this.scrollObserver.observe(this.scrollMarker);
+  }
+
+  setOpaque() {
+    this.opaque = true;
+    document.getElementById('desktop-navbar').classList.add('opaque');
+  }
+  setTransparent() {
+    this.opaque = false;
+    document.getElementById('desktop-navbar').classList.remove('opaque');
+  }
 }
