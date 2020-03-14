235e8a32e066ca09de5a6f6bd91db372161b4504
Matt Hudson <mrh4hd@mail.missouri.edu>
2020-03-14 15:10:44 -0500

    Add unified data component basics

---
+++ fatplant/src/app/components/pages/datapages/unified-datapage/unified-datapage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unified-datapage',
+  templateUrl: './unified-datapage.component.html',
+  styleUrls: ['./unified-datapage.component.scss']
+})
+export class UnifiedDatapageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
