a888fe71445585a8d47d43f878b19f33835efe96
Matt Hudson <mrh4hd@mail.missouri.edu>
2020-04-11 18:06:04 -0500

    Add soybean refseq page fix

---
+++ fatplant/src/app/interfaces/Soybean.ts
@@ -0,0 +1,13 @@
+export interface Soybean {
+    Alternativegenenames: string;
+    Genenames: string;
+    Length: number;
+    ProteinEntry: string;
+    Proteinnames: string;
+    RefSeq: string;
+    RefSeqList: string[];
+    Sequence: string;
+    Species: string;
+    UniprotID: string;
+    names: string;
+}
