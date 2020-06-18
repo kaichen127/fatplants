426b9eb1b90d436be4f691ae57e3974cf325a225
Matt Hudson <mrh4hd@mail.missouri.edu>
2020-06-17 20:06:25 -0500

    Add tair ids to datapage

--- fatplant/src/app/components/pages/datapages/unified-datapage/unified-datapage.component.ts
+++ fatplant/src/app/components/pages/datapages/unified-datapage/unified-datapage.component.ts
@@ -86,7 +86,7 @@ export class UnifiedDatapageComponent implements OnInit {
   get displayedColumns(): String[] {
     switch(this.dataset) {
       case "arabidopsis":
-        return ['uniprot_id','refseq_id','gene_name','gene_symbol','protein_entry','protein_name'];
+        return ['uniprot_id','refseq_id','tair_ids','gene_name','gene_symbol','protein_entry','protein_name'];
       case "camelina":
         return ['camelina','aralip_pathways','ath_description','no','homeologs'];
       case "soybean":
