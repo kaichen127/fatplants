1c442c2f5ac17947b7baaa8a822606d771ab9fa7
Matt Hudson <mrh4hd@mail.missouri.edu>
2020-06-25 17:13:10 -0500

    Add new protein detail component

---
+++ fatplant/src/app/components/pages/datapages/protein-detail/protein-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-protein-detail',
+  templateUrl: './protein-detail.component.html',
+  styleUrls: ['./protein-detail.component.scss']
+})
+export class ProteinDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
