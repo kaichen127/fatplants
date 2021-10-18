c93dced303239169245c161eab2b6127bcfe62da
Michael Fisher <mefisher2@cougars.ccis.edu>
2021-10-18 16:59:06 -0500

    merging in old fatplant dev_michael code to new repo

---
+++ fatplant/src/app/services/firestore-access/firestore-access.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreAccessService {
+
+  constructor(private afs: AngularFirestore) { }
+
+  get(database: string, field: string, query : string, count : number = 1, all : boolean = false)
+  {
+    // console.log(database, field, query);
+    let items : Observable<unknown[]>;
+    if (all)
+    {
+      items = this.afs.collection(database, ref => ref.where(field, '==', query)).valueChanges();
+    }
+    else if (count == 1) 
+    {
+      items = this.afs.collection(database, ref => ref.limit(count).where(field, '==', query)).valueChanges();
+    }
+    else
+    {
+      items = this.afs.collection(database, ref => ref.limit(count).where(field, '>=', query).where(field, '<=', query + '\uf8ff')).valueChanges();
+    }
+    // items.subscribe(data => console.log(data));
+    return items;
+
+  }
+}
